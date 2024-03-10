b94c2e36d679e1ef6ba0c770e076def5702ee3a0
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-03-10 12:45:17 +0100

    add table and class

--- index.php
+++ index.php
@@ -1,7 +1,41 @@
 <?php
+// INCLUDE
 
 
 
+
+
+
+// CLASS
+
+    class Production {
+
+        public $title_name;
+        public $language_movie;
+        public $vote_rate;
+
+        public function __construct($title_name, $language_movie, $vote_rate) {
+            $this->title_name = $title_name;
+            $this->language_movie = $language_movie;
+            $this->vote_rate = $vote_rate;
+        }
+    };
+
+
+    // class Title {
+    //     public $title;
+    // };
+
+    // class Language {
+    //     public $language;
+    // };
+
+
+    // class Vote{
+    //     public $title;
+    // };
+    
+    $Production_1 = new Production ('test', "test", "test");
 
 ?>
 
@@ -19,7 +53,44 @@
 </head>
 <body>
     
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+        <th scope="row">
+            1
+        </th>
+            <td>
+                <?= $Production_1->title_name ?>
+            </td>
+            <td>
+                <?= $Production_1->language_movie ?>
+            </td>
+            <td>
+                <?= $Production_1->vote_rate ?>
+            </td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row"></th>
+      <td colspan="2"></td>
+      <td></td>
+    </tr>
+  </tbody>
 
+</table>
 
 
 
