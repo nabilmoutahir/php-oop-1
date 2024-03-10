16f96bbbd1152d5daafab56fec29b0d376ff7bee
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-03-10 14:18:16 +0100

    split datas

---
+++ data/titles.php
@@ -0,0 +1,14 @@
+<?php
+
+$movies = [
+    "Dragon Ball: Curse of the Blood Rubies",
+    "Dragon Ball: Sleeping Princess in Devil's Castle",
+    "Dragon Ball: Mystical Adventure",
+    "Dragon Ball Z: Dead Zone",
+    "Dragon Ball Z: The World's Strongest",
+    "Dragon Ball Z: The Tree of Might",
+    "Dragon Ball Z: Lord Slug",
+    "Dragon Ball Z: Cooler's Revenge",
+    "Dragon Ball Z: The Return of Cooler",
+    "Dragon Ball Z: Super Android 13!",
+];
